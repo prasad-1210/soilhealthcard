b2a4f251c7d268efbf428d421514c5bcd2eb3400
Prasad CH <vkprasad.ch.ags@gmail.com>
2017-04-06 13:25:03 +0800

    multiple fert suggestions suppressed

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -12,9 +12,9 @@ module.exports = {
     }, (err, uploadedFiles) => {
       if (err) return res.serverError(err);
 	    console.log(err, uploadedFiles[0].fd);
-
       fs.readFile(uploadedFiles[0].fd, function(err, data) {
           var json = JSON.parse(parser.toJson(data));
+          console.log(JSON.stringify(json));
           res.view('file/layout',{ data : json.Report });
       });
   });
