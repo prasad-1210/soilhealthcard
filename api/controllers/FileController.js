d03cfc37816c143123ce53d792621b68128654b9
Prasad CH <vkprasad.ch.ags@gmail.com>
2017-04-15 01:04:35 +0800

    code clean up and json reader for ease of vieweing xml in json

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -15,9 +15,25 @@ module.exports = {
       fs.readFile(uploadedFiles[0].fd, function(err, data) {
           var json = JSON.parse(parser.toJson(data));
           console.log(JSON.stringify(json));
-          res.view('file/layout',{ data : json.Report });
+          res.view('print',{ data : json.Report });
       });
   });
   },
+  convertToJson(req, res) {
+    var csvData = [];
+    res.setTimeout(0);
+    req.file('avatar')
+    .upload({
+      maxBytes: 100 * 1000000
+
+    }, (err, uploadedFiles) => {
+      if (err) return res.serverError(err);
+	    console.log('convertToJson: ', err, uploadedFiles[0].fd);
+      fs.readFile(uploadedFiles[0].fd, function(err, data) {
+          var json = JSON.parse(parser.toJson(data));
+          res.view('jsonviewer',{json});
+      });
+  });
+  }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -5,5 +5,18 @@ module.exports.routes = {
     locals: {
      	layout: false
     }
+  },
+  '/jsonreader': {
+    view: 'jsonreader',
+    locals: {
+     	layout: false
+    }
+  },
+  'convert':{
+    controller: "FileController",
+    action: "convertToJson",
+    locals: {
+      layout: false
+    }
   }
 };
